ac57baba6c730e929341f7edf66722597ee01121
phisaz <phisaz@d28545a4-134c-0410-a928-79294955dc5f>
2008-05-14 14:44:36 +0000

    print features added: date, file size, file size sum, file count, dir count

--- branches/antonio/dir.c
+++ branches/antonio/dir.c
@@ -1,5 +1,27 @@
+/*
+ * authors: Sergio Matone & Antonio Vetrò
+ * tutor: Marco Murciano
+ * group: Politecnico di Torino - System Programming
+ *
+ ** ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and  limitations under the
+ * License.
+ *
+ * ***** END LICENSE BLOCK ***** 
+ */
+
 //TODO recursion algorithm
 //TODO printout formatting functions
+//TODO getline realloc: maybe unsigned int length = u + strlen(str); without strlen
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,9 +31,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>  
 #include <dirent.h>
+#include <semaphore.h>
 #include "resource.h"
 //TODO better definition of maximum path length
-#define MAXPATH 350
+
+//Looking in /usr/src/linux-2.4.20-8/include/linux/limits.h, I see:
+//#define PATH_MAX 4096 /* # chars in a path name including nul */
+//TODO define globally for all files
+#define MAXPATH 4096
 /* 
  * D  Directory, R  File sola lettura, H  File nascosti
  * A  File archivio, S  File di sistema, - Prefisso per negare l'attributo
@@ -31,132 +58,157 @@ short show_recursive; // /S option
 Resource *resources_list;
 
 //it initialize tha mask with default options
-void initialize(){
-	 	 
-	 show_directory = 1;
-     show_read_only = 0; 
-     show_hidden_files = 0;
-	 show_system_files = 0; //TODO do "system" files exist under linux ?
-	 show_archive_files = 1;//TODO do archive files exist under linux ?
+void initialize() {
 
-	 show_owner = 0; // /Q option 
-	 show_recursive = 0; // /S option
-	 resources_list = NULL;
+	show_directory = 1;
+	show_read_only = 0;
+	show_hidden_files = 0;
+	show_system_files = 0; //TODO do "system" files exist under linux ?
+	show_archive_files = 1;//TODO do archive files exist under linux ?
+
+	show_owner = 0; // /Q option 
+	show_recursive = 1; // /S option //TODO to change in 0
+	resources_list = NULL;
+
+}
+
+Resource *create_res(struct stat status, char res_name[], unsigned char type) {
+
+	Resource *res;
+	char *name;
+
+	name =(char *) malloc((unsigned int)strlen(res_name));
+	strcpy(name, res_name);
+
+	res = new_resource();
+	res->status = status;
+	res->name = name;
+	res->type = type;
+	return res;
+
+}
+
+Resource *my_dir(char *path) {
+
+	DIR *dp;
+	Resource *res;
+	Resource *to_print, *temp;
+
+	struct dirent *ep;
+	struct stat status;
+	char current_dir[MAXPATH], temp_path[MAXPATH];
+	char *res_name;
+	int *p;
+
+	strcpy(current_dir, path);
+
+	dp = opendir(current_dir);
+	if (dp != NULL) {
+		while (ep = readdir(dp)) {
+
+			//ep->type: 4 dir, 8 file
+			if (show_directory==0 && ep->d_type==4) {
+				continue;
+			}
+
+			//if ep = . or .. , i don't need to process the node
+			
+			if ( (strcmp(ep->d_name, ".") == 0 ) || (strcmp(ep->d_name, "..")
+					== 0)) {
+				//printf("%s\n",ep->d_name);
+				continue;
+			}
+			if (show_hidden_files==0 && ep->d_name[0]=='.') {
+				continue;
+			}
+
+			//at every round, i reset the temp_path to current dir value
+			strcpy(temp_path, current_dir);
+
+			//if the current dir doesn't finish with '/',i should add it before appending the currente resource name
+			if ( ((char)current_dir[strlen(current_dir)-1]) != '/') {
+
+				strcat(temp_path, "/");
+
+			}
+			strcat(temp_path, ep->d_name);
+
+			//TODO recursion
+			/*
+			 if(show_recursive==1 && ep->d_type==4){
+			 my_dir(temp_path);
+			 }*/
+
+			//TODO files that terminates with "~" aren't added to the list
+			if (temp_path[strlen(temp_path)-1] == '~') {
+				//printf("%s\n",ep->d_name);
+				continue;
+			}
+
+			if ( (p=open(temp_path, O_EXCL)) == NULL) {
+
+				printf("dir: cannot access : %s: No such file or directory\n",
+						temp_path);
+				continue;
+			}
+
+			if (fstat(p, &status) != 0) {
+				if (ep->d_type==4)
+					printf("Cannot open directory %s: Permission denied\n",
+							temp_path);
+				else
+					printf("Cannot open file %s: Permission denied\n",
+							temp_path);
+				continue;
+
+			}
+
+			//st.mode 400 (user read only)-> 33024
+			//TODO should i menage other read only cases ?
+			if (show_read_only==1
+					&& (((unsigned short)status.st_mode) == 33024))
+				continue;
+
+			res = create_res(status, ep->d_name, ep->d_type);
+			insert_resource(&resources_list, res);
+
+		}
+		(void) closedir(dp);
+		/*
+		while (resources_list!= NULL) {
+			insert_resource(&to_print, get_resource(&resources_list));
+		}
+		print_list(to_print, current_dir);
+		*/
+	} else{
+		printf("dir: cannot access : %s: No such file or directory\n", path);
+		//return to_print;//TODO correct
+	}	
+	return resources_list;
+}
+
+int main(int argc, char **argv) {
+
+	Resource *to_print;
+	Resource *to_dir;
 	 
+	char current_dir[MAXPATH];
+
+	if (argc != 2) {
+		printf("Please specify one argument\n");
+		return -1;
+	}
+
+	//TODO initialize with correct parameters/options
+	initialize();
+
+	strcpy(current_dir, argv[1]);
+	
+	
+	to_print = my_dir(current_dir);
+	print_list(to_print, current_dir);
+	
+	
+	
+	return 0;
 }
-
-Resource *create_res(struct stat status, char *res_name){
-		
-		Resource *res;
-		
-		res = new_resource();
-		res->status = status;
-		res->name = res_name;
-		return res;
-		
-}
-
-int
-main (int argc, char **argv)
-{
-  
-  DIR *dp;
-  Resource *res;
-  struct dirent *ep;
-  struct stat status;
-  char current_dir[MAXPATH], temp_path[MAXPATH];
-  char process_node='1'; //tell us wheter i have to process(=visualize) the node or not
-  int *p;
-  off_t size;
-  
-  
-  if(argc != 2){
-	  printf("Please specify one argument\n");
-	  return -1;
-  }
-  
-  initialize();
-  
-  strcpy(current_dir,argv[1]);
-  
-  dp = opendir (current_dir);
-  if (dp != NULL)
-    {
-      while (ep = readdir (dp)){
-    	 
-    	  
-    	  //if ep = . or .. , i don't need to process the node
-    	  if( (strcmp(ep->d_name,".") == 0 ) || ( strcmp(ep->d_name,"..") == 0) ){
-    	     		  printf("%s\n",ep->d_name);
-    	     		  continue;
-    	  }
-    	  if(show_hidden_files==0 && ep->d_name[0]=='.'){
-    		  continue;
-    	  }
-    	  //ep->type: 4 dir, 8 file
-    	  if(show_directory==0 && ep->d_type==4){
-    		  continue;
-    	  }
-    	  //at every round, i reset the temp_path to current dir value
-    	  strcpy(temp_path,current_dir);
-    	    	  
-    	  //if the current dir doesn't finish with '/',i should add it before appending the currente resource name
-    	  if( ((char)current_dir[strlen(current_dir)-1]) != '/'){
-    		  
-    		  strcat(temp_path,"/");
-    	  		      	  
-    	  }
-       	  
-    		  strcat(temp_path,ep->d_name);
-    	      	     	  
-    	  //TODO try to understand why files that terminates with "~" (temporary files?) aren't acessible by fstat
-    	  //files that terminates with "~" (temporary files?) aren't acessible by fstat
-    	  //TODO decide wheter files that terminates with "~" should be added to the list
-    	  if(temp_path[strlen(temp_path)-1] == '~'){
-    		  printf("%s\n",ep->d_name);
-    		  continue;
-    	  }
-    	  
-    	  if( (p=open(temp_path,O_EXCL)) == NULL){
-    		  
-    		  printf("Invalid path : %s\n",temp_path);
-    		  return -1;
-    	  }
-    	  
-    	  //fstat: these functions returns information about a file. No permissions are required on the file itself, 
-    	  //but -- in the case of stat() and lstat() -- execute (search) permission is required on all of the 
-    	  //directories in path that lead to the file.
-    	  //TODO privilege problems : print "ls: cannot open directory /home/lost+found/: Permission denied"
-
-    	  if(fstat(p,&status) != 0){
-		    		      			  
-		    			  printf("Can't retrieve information from resource %s\n", temp_path);
-		    			  //return -1;
-		    			  continue;
-		    		    		
-		  }
-    	  //st.mode 400 (user read only)-> 33024
-    	  //TODO should i menage other read only cases ?
-    	  
-    	  if( show_read_only==1 && (((unsigned short)status.st_mode) == 33024) )
-    		  continue;
-    	  
-    	  res = create_res(status,ep->d_name);
-		  insert_resource(&resources_list,res);
-		  
-      	  
-    	  printf("%s \t %lu\n", ep->d_name,(unsigned long)status.st_size);
-    	  
-    	  
-    	  
-      }
-      (void) closedir (dp);
-    }
-  else
-    printf("Couldn't open the directory.\n");
-
-  
-  print_list(resources_list);
-  return 0;
-}
-
--- branches/antonio/resource.h
+++ branches/antonio/resource.h
@@ -1,3 +1,25 @@
+/*
+ * authors: Sergio Matone & Antonio Vetrò
+ * tutor: Marco Murciano
+ * group: Politecnico di Torino - System Programming
+ *
+ ** ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and  limitations under the
+ * License.
+ *
+ * ***** END LICENSE BLOCK ***** 
+ */
+
+
 #include <sys/stat.h>  
 
 typedef struct resource_unit Resource;
@@ -8,12 +30,13 @@ struct resource_unit
 	Resource *next;	/* Next event in the list */
 	Resource *prev;    /* Previous event in the list */
 	char *name;     /* name of the resource */
-	//int   type;     /* Type of event, to be defined */
+	unsigned char type;     /* Type of resource, 4 =  directory , 8 = file */
 	/* Optional fields, depending on the events to be simulated */
 	//int   param1;   /* Generic integer parameter */
 	//int   param2;   /* Generic integer parameter */
 	//double param3;  /* Generic double  parameter */
 	struct stat status; /* The result of the fstatus */
+	
 	//char  *record; /* Generic pointer parameter, to be used  through a cast to the specific type   */
 	//Record *record;
       };
@@ -24,4 +47,4 @@ Resource *get_resource(Resource **);		/* Returns the first event from the FES */
 Resource *new_resource(void );		/* Returns a new event */
 void release_resource(Resource *);		/* Releases an event */
 void delete_resource(Resource **,Resource *); /* Delete a resource */
-void print_list(Resource *);
+void print_list(Resource *, char *);
